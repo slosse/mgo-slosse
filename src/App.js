1bbf437a3b02d8902e3c2cf6768aa2c17aac7df7
slosse <enriqueslosse@gmail.com>
2021-08-29 20:53:21 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          mgo
-        </p>
-      </header>
+    <div className="App" >
+      <NavBar/> 
     </div>
   );
 }
